8ddddfaaaeb6c36b4c0f2cbf229f9cdb0c24f1f6
codefug <robot9917@naver.com>
2023-12-31 18:05:38 +0900

    complete npm publish!!

--- src/DropDownLogic/js/makeMenu.js
+++ src/DropDownLogic/js/makeMenu.js
@@ -1,16 +1,34 @@
-function revealItem(target) {
-  const items = Array.from(target.parentNode.childNodes).filter(
-    (_, value) => value % 2 === 1 && value !== 1,
+// function revealItem(target) {
+//   const items = Array.from(target.parentNode.childNodes).filter(
+//     (_, value) => value % 2 === 1 && value !== 1,
+//   );
+//   items.forEach((element) => {
+//     element.setAttribute("style", "display:block");
+//   });
+// }
+
+// function removeItem(target) {
+//   Array.from(target.childNodes)
+//     .filter((_, value) => value % 2 === 1 && value !== 1)
+//     .forEach((value) => value.setAttribute("style", "display:none"));
+// }
+
+function revealtext() {
+  Array.from(document.querySelectorAll(".dropDownItemDescription")).forEach(
+    (element) => {
+      element.setAttribute("style", "display:inline");
+    },
   );
-  items.forEach((element) => {
-    element.setAttribute("style", "display:inline-block");
-  });
 }
 
-function removeItem(target) {
-  Array.from(target.childNodes)
-    .filter((_, value) => value % 2 === 1 && value !== 1)
-    .forEach((value) => value.setAttribute("style", "display:none"));
+function removetext(){
+    Array.from(document.querySelectorAll(".dropDownItemDescription")).forEach(
+      (element) => {
+        element.setAttribute("style", "display:none");
+      },
+    );
 }
+export { revealtext, removetext };
+// export { revealItem, removeItem };
 
-export { revealItem, removeItem };
+//  hover event using JS
--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import "./DropDownLogic/css/stylemenu.css";
-import { removeItem, revealItem } from "./DropDownLogic/js/makeMenu";
+import { removetext, revealtext } from "./DropDownLogic/js/makeMenu";
+// import { removeItem, revealItem } from "./DropDownLogic/js/makeMenu";
+// (function start() {
+//   const menuButton = document.querySelector(".menuButton");
+//   const menu = document.querySelector(".dropDownMenu");
+//   menuButton.addEventListener("pointerover", (event) => {
+//     revealItem(event.target);
+//   });
+//   menu.addEventListener("pointerleave", (event) => {
+//     removeItem(event.target);
+//     event.stopPropagation();
+// });
+// })();
+
+//  hover event using JS
 
 (function start() {
-  const menuButton = document.querySelector(".menuButton");
-  const menu = document.querySelector(".dropDownMenu");
-  menuButton.addEventListener("pointerover", (event) => {
-    revealItem(event.target);
+     const menuButton = document.querySelector(".menuButton");
+    const menu = document.querySelector(".dropDownMenu");
+     const dropDownItemDescription = Array.from(
+    document.querySelectorAll(".dropDownItemDescription"),
+  );
+  dropDownItemDescription.forEach((value) =>
+    value.setAttribute("style", "display:none"),
+  );
+  menuButton.addEventListener("pointerover",()=>{
+    setTimeout(revealtext, 400);
+  })
+  menu.addEventListener("pointerleave", () => {
+    removetext();
   });
-  menu.addEventListener("pointerleave", (event) => {
-    removeItem(event.target);
-    event.stopPropagation();
-});
 })();
